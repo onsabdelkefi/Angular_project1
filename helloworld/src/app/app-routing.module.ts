839e2820957e08bf004fda1c9b8c0d6d54db0e15
onsabdelkefi <ons.abdelkafi@enis.tn>
2023-10-10 22:30:30 +0200

    helloworld

---
+++ helloworld/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TestComponent } from './test/test.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {path:'test',component:TestComponent},
+  {path:'home',component:HomeComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
